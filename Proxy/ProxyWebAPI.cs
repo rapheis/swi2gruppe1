0f9f6116686d18438116f6ec2d8f502ca898dd85
rapheis <91463137+rapheis@users.noreply.github.com>
2022-03-06 18:20:56 +0100

    Erster Echter Commit

---
+++ Proxy/ProxyWebAPI.cs
@@ -0,0 +1,23 @@
+﻿namespace swi2gruppe1.Proxy
+
+{
+    using swi2gruppe1.Data;
+    public class ProxyWebAPI
+    {
+        public static WeatherForecast GetWeatherForecast()
+        {
+            Console.WriteLine("hier wäre der WebAPI Get-Aufruf...");
+            WeatherForecast forecast = new WeatherForecast();
+            forecast.TemperatureC = 25;
+            forecast.Date = DateTime.Now;
+            forecast.Summary = "Wetter von St. Gallen";
+            return forecast;
+        }
+        public static void SendWeatherForecast(WeatherForecast forecast)
+        {
+            Console.WriteLine("hier würde das WebAPI aufgerufen.." + forecast.TemperatureC +
+                " " + forecast.Summary);
+        }
+    }
+}
+
